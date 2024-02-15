5aa12daf8de3e7927fe795f2815062c4873a84c3
Nadya159 <nadezdovna59@gmail.com>
2024-02-15 11:38:34 +0500

    Change annotation

--- src/main/java/by/javaguru/spring/service/CompanyService.java
+++ src/main/java/by/javaguru/spring/service/CompanyService.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 public class CompanyService {
     private final CompanyRepository companyRepository;
 
+    @Autowired
     public CompanyService(CompanyRepository companyRepository) {
         this.companyRepository = companyRepository;
     }
--- src/main/java/by/javaguru/spring/service/UserService.java
+++ src/main/java/by/javaguru/spring/service/UserService.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 public class UserService {
     private final UserRepository userRepository;
 
+    @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
